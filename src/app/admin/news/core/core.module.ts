849ef92f10b90f77cef92b0ea89206454a6172b0
jhonatanbg27 <jhonatanbg@gmail.com>
2019-11-22 00:23:46 -0500

    add table form

--- src/app/admin/news/core/core.module.ts
+++ src/app/admin/news/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewTableComponent } from './components/new-table/new-table.component';
+import { NewFormComponent } from './components/new-form/new-form.component';
 
 
 
 @NgModule({
-  declarations: [NewTableComponent],
+  declarations: [NewTableComponent, NewFormComponent],
   imports: [
     CommonModule
   ]
