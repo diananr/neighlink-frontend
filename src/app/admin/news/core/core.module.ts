8481d835e903f5cafa0d550ffeac87421ee88a95
Diana Navarro <navarroruiz.diana@gmail.com>
2019-11-22 12:59:15 +0100

    improve name

--- src/app/admin/news/core/core.module.ts
+++ src/app/admin/news/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NewTableComponent } from './components/new-table/new-table.component';
-import { NewFormComponent } from './components/new-form/new-form.component';
-
-
+import { NewFormComponet } from './components/new-form/new-form.component';
 
 @NgModule({
-  declarations: [NewTableComponent, NewFormComponent],
+  declarations: [
+    NewTableComponent,
+    NewFormComponet
+  ],
   imports: [
     CommonModule
   ]
